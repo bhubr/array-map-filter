76b5a6dfeb4a03278aeeebad1751204598cc8da2
Benoît Hubert <benoithubert@gmail.com>
2018-09-30 12:25:44 +0200

    Add 3rd filter exercise

---
+++ filter/ex3.js
@@ -0,0 +1,30 @@
+/* Array.prototype.filter - Exercice 3
+
+Ecrire une fonction keepStarks, qui prend en arguments un tableau de chaînes
+(plus spécifiquement, des noms de personnages de Game of Thrones).
+
+La fonction doit renvoyer un tableau ne contenant que les membres de la famille Stark.
+
+Il y a plusieurs façons d'écrire cette fonction, et outre filter, tu auras probablement
+besoin d'une de ces méthodes de String: endsWith ou split.
+
+Exemple d'entrée:
+  [
+    'Bran Stark',
+    'Cersei Lannister',
+    'Sandor Clegane',
+    'Arya Stark',
+    'Yara Greyjoy',
+    'Sansa Stark'
+  ]
+
+Sortie attendue:
+  ['Bran Stark', 'Arya Stark', 'Sansa Stark']
+
+ */
+
+function keepStarks(names) {
+}
+
+// Ne pas modifier l'export
+module.exports = keepStarks;
---
+++ tests/filter3.test.js
@@ -0,0 +1,18 @@
+const keepStarks = require('../filter/ex3.js');
+
+xdescribe('filter - exercice 3', () => {
+
+  test('keepStarks', () => {
+    expect(keepStarks([
+      'Bran Stark',
+      'Cersei Lannister',
+      'Sandor Clegane',
+      'Arya Stark',
+      'Yara Greyjoy',
+      'Sansa Stark'
+    ]))
+    .toEqual(['Bran Stark', 'Arya Stark', 'Sansa Stark']);
+  });
+
+});
+
