e989ad82be45a9e6b469564d846132307ceeb035
Louis Bruge <louis.bruge@gmail.com>
2018-11-24 15:19:36 +0100

    Fix (map) : ex1

--- map/ex1.js
+++ map/ex1.js
@@ -22,6 +22,9 @@ En sortie: [
  */
 
 function getStringsLength(strings) {
+  return strings.map(word => {
+    return `${word} contains ${word.length} characters`;
+  });
 }
 
 // Si besoin, tu peux toujours visualiser le résultat avec console.log,
