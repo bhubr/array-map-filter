8425a6def5e06fe37e6ceb04cfcda8e0a521e14a
Louis Bruge <louis.bruge@gmail.com>
2018-11-24 14:24:00 +0100

    Fix (filter) : ex1

--- filter/ex1.js
+++ filter/ex1.js
@@ -12,6 +12,9 @@ Sortie attendue:
  */
 
 function getPositiveNumbers(numbers) {
+  return numbers.filter((number) => {
+    return number >= 0;
+  });
 }
 
 // Si besoin, tu peux toujours visualiser le résultat avec console.log,
