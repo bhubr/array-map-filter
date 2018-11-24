ba771283aaa68056db6143151a02e7b5f242e8f9
Louis Bruge <louis.bruge@gmail.com>
2018-11-24 14:53:35 +0100

    Fix (filter) : ex5

--- filter/ex5.js
+++ filter/ex5.js
@@ -39,6 +39,9 @@ Exemple d'entrée:
  */
 
 function searchWordFilter(items, search) {
+  return items.filter(item => {
+      return item.toLowerCase().includes(search.toLowerCase());
+  });
 }
 
 // Ne pas modifier l'export
